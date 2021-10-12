801bd5fd8ad9e300d8ee382c6a1984fcbb1c21a1
Bl4ckmed <57529768+Bl4ckmed@users.noreply.github.com>
2021-10-13 00:39:28 +0200

    Added increment value to increment function

--- Counter_With_Child_Components_props_stateHook.js
+++ Counter_With_Child_Components_props_stateHook.js
@@ -1,33 +1,31 @@
-
 function Button(props) {
-  
+  const handleClick = () => props.onClickFunction(props.increment) ;
 	return (
-    <button onClick={props.changeCounter}> Increment counter value </button>
-    );
+  	<button onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
 }
-
 
 function Display(props) {
-  
 	return (
-    <div>value : {props.message} </div>
-    );
+  	<div>{props.message}</div>
+  );
 }
-
 
 function App() {
-  
-  const [counter, setCounter]=useState(0) 
-  const incrementor = () =>  setCounter(counter+1)
-  
+	const [counter, setCounter] = useState(0);
+  const incrementCounter = (value) => setCounter(counter+value);
 	return (
     <div>
-      <Button changeCounter={incrementor}/>
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={100} />
       <Display message={counter}/>
-    </div>
-    );
+    </div>  
+  );
 }
-
 
 ReactDOM.render(
   <App />, 
